e08d844383f2d1b7640ab71771788d4222cb8cbe
康永帅 <yongshuai.kang@zhaopin.com.cn>
2020-03-18 11:15:44 +0800

    arm64

--- ZPMLagMonitor/Classes/Trace/SMCallTraceCore.c
+++ ZPMLagMonitor/Classes/Trace/SMCallTraceCore.c
@@ -9,7 +9,7 @@
 #include "SMCallTraceCore.h"
 
 // __aarch64__ arm64 架构又分为 2 种执行状态：AArch64 Application Level 和 AArch32 Application Level
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -48,7 +48,7 @@ __attribute__((__naked__)) static void hook_Objc_msgSend(void); // objc_msgSend 
 
 /// 开始监听并进行 objc_msgSend 实现替换
 void smCallTraceStart(void) {
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
     _call_record_enabled = true;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -62,7 +62,7 @@ void smCallTraceStart(void) {
 
 /// 停止监听
 void smCallTraceStop(void) {
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
     _call_record_enabled = false;
 #endif
 }
@@ -70,7 +70,7 @@ void smCallTraceStop(void) {
 /// 开始监听并设置最小耗时
 /// @param us 最小耗时 单位纳秒 us，默认 1000 us
 void smCallConfigMinTime(uint64_t us) {
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
     _min_time_cost = us;
 #endif
 }
@@ -78,7 +78,7 @@ void smCallConfigMinTime(uint64_t us) {
 /// 开始监听并设置栈最大调用深度
 /// @param depth 栈最大调用深度，默认 3
 void smCallConfigMaxDepth(int depth) {
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
     _max_call_depth = depth;
 #endif
 }
@@ -86,7 +86,7 @@ void smCallConfigMaxDepth(int depth) {
 /// 回去方法耗时数据
 /// @param num 数据条数
 smCallRecord *smGetCallRecords(int *num) {
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
     if (num) {
         *num = _smRecordNum;
     }
@@ -101,7 +101,7 @@ smCallRecord *smGetCallRecords(int *num) {
 
 /// 清空监听数据
 void smClearCallRecords(void) {
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
     if (_smCallRecords) {
         free(_smCallRecords);
         _smCallRecords = NULL;
@@ -114,7 +114,7 @@ void smClearCallRecords(void) {
 #pragma mark - Record
 
 // arm64架构又分为2种执行状态： AArch64 Application Level 和 AArch32 Application Level
-#if __arm64__  ||  (__x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC)
+#if __arm64__
 
 typedef struct {
     id self;            // 通过 object_getClass 能够得到 Class 再通过 NSStringFromClass 能够得到类名
@@ -217,9 +217,11 @@ uintptr_t after_objc_msgSend() {
 
 #endif
 
-#if __arm64__
 
 #pragma mark - 64 位真机实现
+
+#if __arm64__
+
 __attribute__((__naked__)) static void hook_Objc_msgSend() {
     // save parameter registers: x0..x8, q0..q7
     __asm volatile (
@@ -288,186 +290,6 @@ __attribute__((__naked__)) static void hook_Objc_msgSend() {
                     );
 
     // return
-    __asm volatile ("ret\n");
-}
-
-#endif
-
-#if __x86_64__  &&  TARGET_OS_SIMULATOR  &&  !TARGET_OS_IOSMAC
-
-#pragma mark - 64 位模拟器实现
-
-/*
-    栈基址指针 %rbp 始终指向当前函数调用开始时栈的位置，栈指针 %rsp 始终指向栈中最新的元素对应的
- 位置， %rbp 和 %rsp 之间的元素被我们成为”栈帧”
- 
-    因为执行函数调用前会将调用者的下一条指令压入栈中，而被调用者函数内部因为有本地栈帧的定义又会将
- 栈顶下移，所以在被调用者函数执行ret指令返回之前需要确保当前堆栈寄存器 %rsp 所指向的栈顶地址要和
- 被调用函数执行前的栈顶地址保持一致，不然当ret指令执行时取出的调用者的下一条指令的值将是错误的，从
- 而会产生崩溃异常
- */
-
-__attribute__((__naked__)) static void hook_Objc_msgSend() {
-    
-//    __asm volatile (
-//                    "pushq  %rbp\n"
-//                    "movq   %rsp, %rbp\n"
-//                    "subq   $0x88, %rsp\n"
-//                    "movdqa %xmm0, -0x80(%rbp)\n"
-//                    "pushq  %rax\n"
-//                    "movdqa %xmm1, -0x70(%rbp)\n"
-//                    "pushq  %rdi\n"
-//                    "movdqa %xmm2, -0x60(%rbp)\n"
-//                    "pushq  %rsi\n"
-//                    "movdqa %xmm3, -0x50(%rbp)\n"
-//                    "pushq  %rdx\n"
-//                    "movdqa %xmm4, -0x40(%rbp)\n"
-//                    "pushq  %rcx\n"
-//                    "movdqa %xmm5, -0x30(%rbp)\n"
-//                    "pushq  %r8\n"
-//                    "movdqa %xmm6, -0x20(%rbp)\n"
-//                    "pushq  %r9\n"
-//                    "movdqa %xmm7, -0x10(%rbp)\n"
-//                    );
-//
-//    __asm volatile (
-//                    "movq       0x8(%rbp), %rdx\n"      // 返回地址
-//                    "call       _before_objc_msgSend\n"
-//                    "movq       %rax, %r11\n"
-//                    );
-//
-//    __asm volatile (
-//                    "movdqa -0x80(%rbp), %xmm0\n"
-//                    "popq   %r9\n"
-//                    "movdqa -0x70(%rbp), %xmm1\n"
-//                    "popq   %r8\n"
-//                    "movdqa -0x60(%rbp), %xmm2\n"
-//                    "popq   %rcx\n"
-//                    "movdqa -0x50(%rbp), %xmm3\n"
-//                    "popq   %rdx\n"
-//                    "movdqa -0x40(%rbp), %xmm4\n"
-//                    "popq   %rsi\n"
-//                    "movdqa -0x30(%rbp), %xmm5\n"
-//                    "popq   %rdi\n"
-//                    "movdqa -0x20(%rbp), %xmm6\n"
-//                    "popq   %rax\n"
-//                    "movdqa -0x10(%rbp), %xmm7\n"
-//                    "testq  %r11, %r11\n"
-//                    "leave\n"
-//                    );
-//
-//    // objc_msgSend
-//    __asm volatile ("addq   $0x8, %rsp\n");
-//    __asm volatile ("call       *%r11\n");
-    
-    
-    __asm volatile (
-                    "subq       $(0x80+0x8), %rsp\n"
-                    
-                    "movdqa     %xmm0, (%rsp)\n"
-                    "movdqa     %xmm1, 0x10(%rsp)\n"
-                    "movdqa     %xmm2, 0x20(%rsp)\n"
-                    "movdqa     %xmm3, 0x30(%rsp)\n"
-                    "movdqa     %xmm4, 0x40(%rsp)\n"
-                    "movdqa     %xmm5, 0x50(%rsp)\n"
-                    "movdqa     %xmm6, 0x60(%rsp)\n"
-                    "movdqa     %xmm7, 0x70(%rsp)\n"
-                    "pushq      %rax\n"
-                    "pushq      %r9\n"
-                    "pushq      %r8\n"
-                    "pushq      %rcx\n"
-                    "pushq      %rdx\n"
-                    "pushq      %rsi\n"
-                    "pushq      %rdi\n"
-                    "pushq      %rax\n"
-                    );
-    
-    __asm volatile (
-                    "call       _before_objc_msgSend\n"
-                    "movq       %rax, %r10\n"
-                    );
-
-    __asm volatile (
-                    "pop        %rax\n"
-                    "pop        %rdi\n"
-                    "pop        %rsi\n"
-                    "pop        %rdx\n"
-                    "pop        %rcx\n"
-                    "pop        %r8\n"
-                    "pop        %r9\n"
-                    "pop        %rax\n"
-                    "movdqa     (%rsp), %xmm0\n"
-                    "movdqa     0x10(%rsp), %xmm1\n"
-                    "movdqa     0x20(%rsp), %xmm2\n"
-                    "movdqa     0x30(%rsp), %xmm3\n"
-                    "movdqa     0x40(%rsp), %xmm4\n"
-                    "movdqa     0x50(%rsp), %xmm5\n"
-                    "movdqa     0x60(%rsp), %xmm6\n"
-                    "movdqa     0x70(%rsp), %xmm7\n"
-                    
-                    "addq       $(16*8+8),  %rsp\n"
-                    );
-
-    // objc_msgSend
-    __asm volatile ("jmpq       *%r10\n");
-    //__asm volatile ("callq       *%0\n" :: "r"(orig_objc_msgSend));
-
-    __asm volatile (
-                    "pushq      %r10\n"
-                    "push       %rbp\n"
-                    "movq       %rsp, %rbp\n"
-                    
-                    "subq       $(0x80), %rsp\n"
-                    "movdqa     %xmm0, -0x80(%rbp)\n"
-                    "push       %rax\n"
-                    "movdqa     %xmm1, -0x70(%rbp)\n"
-                    "push       %rdi\n"
-                    "movdqa     %xmm2, -0x60(%rbp)\n"
-                    "push       %rsi\n"
-                    "movdqa     %xmm3, -0x50(%rbp)\n"
-                    "push       %rdx\n"
-                    "movdqa     %xmm4, -0x40(%rbp)\n"
-                    "push       %rcx\n"
-                    "movdqa     %xmm5, -0x30(%rbp)\n"
-                    "push       %r8\n"
-                    "movdqa     %xmm6, -0x20(%rbp)\n"
-                    "push       %r9\n"
-                    "movdqa     %xmm7, -0x10(%rbp)\n"
-                    
-                    "pushq      0x8(%rbp)\n"
-                    "movq       %rbp, %rax\n"
-                    "addq       $8, %rax\n"
-                    "pushq      %rax\n"
-                    );
-
-    __asm volatile (
-                    "call       _after_objc_msgSend\n"
-                    );
-    
-    __asm volatile (
-                    "pop        %rax\n"
-                    "pop        8(%rbp)\n"
-                    
-                    "movdqa     -0x80(%rbp), %xmm0\n"
-                    "pop        %r9\n"
-                    "movdqa     -0x70(%rbp), %xmm1\n"
-                    "pop        %r8\n"
-                    "movdqa     -0x60(%rbp), %xmm2\n"
-                    "pop        %rcx\n"
-                    "movdqa     -0x50(%rbp), %xmm3\n"
-                    "pop        %rdx\n"
-                    "movdqa     -0x40(%rbp), %xmm4\n"
-                    "pop        %rsi\n"
-                    "movdqa     -0x30(%rbp), %xmm5\n"
-                    "pop        %rdi\n"
-                    "movdqa     -0x20(%rbp), %xmm6\n"
-                    "pop        %rax\n"
-                    "movdqa     -0x10(%rbp), %xmm7\n"
-                    
-                    "leave\n"
-                    "movq       %r10, (%rsp)\n"
-                    );
-
     __asm volatile ("ret\n");
 }
 
